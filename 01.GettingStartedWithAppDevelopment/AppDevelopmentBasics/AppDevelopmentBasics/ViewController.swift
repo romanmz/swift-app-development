c74eabea20da28ac3b82be35e241c1961ab36554
Román Martínez <roman.mx3@gmail.com>
2019-03-04 21:10:53 -0600

    Moved info from "GettingStarted" to "AppDevelopmentBasics"

--- 01.GettingStartedWithAppDevelopment/AppDevelopmentBasics/AppDevelopmentBasics/ViewController.swift
+++ 01.GettingStartedWithAppDevelopment/AppDevelopmentBasics/AppDevelopmentBasics/ViewController.swift
@@ -13,8 +13,21 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		// ++++++++++++++++++++++++++++++
+		testBreakpoints()
 	}
-
-
+	
+	// Use breakpoints to stop the execution of the app at a given point
+	// you can then analyse step by step what's happening on the app
+	func testBreakpoints() {
+		var names = ["Tammy", "Cole"]
+		names.removeFirst()
+		names.removeFirst()
+	}
+	
+	// Triggered when the app is running out of memory
+	// use it to dispose of any resources that can be easily re-created
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
 }
-
