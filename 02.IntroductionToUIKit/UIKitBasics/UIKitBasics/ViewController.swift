19995e92cfe57cd6fe72d1e2d859c91674794c3b
Román Martínez <roman.mx3@gmail.com>
2019-03-04 22:01:55 -0600

    Created "UIKitBasics" project

---
+++ 02.IntroductionToUIKit/UIKitBasics/UIKitBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitBasics
+//
+//  Created by Román Martínez on 3/4/19.
+//  Copyright © 2019 Román Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
