2776c34a2e2a6b3e91f39b96b4f4cc88e222a603
Román Martínez <roman.mx3@gmail.com>
2019-03-12 19:02:26 -0600

    Moved info from "FlyingSquares" into "PracticalAnimation"

--- 05. Working with the web/PracticalAnimation/PracticalAnimation/ViewController.swift
+++ 05. Working with the web/PracticalAnimation/PracticalAnimation/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		flyingSquare()
 	}
-
-
+	
+	
+	// Transform and colour animations
+	// ------------------------------
+	// Use the UIView.animate() method to animate multiple properties of an element at once
+	// the final callback is applied after the initial animation ends, you can concatenate extra animations here
+	// Use the CGAffineTransform class to easily handle transforms
+	func flyingSquare() {
+		
+		// Create square
+		let square = UIView(frame: CGRect(x: 0, y:0, width: 100, height: 100))
+		square.backgroundColor = .purple
+		view.addSubview(square)
+		
+		// Animate
+		UIView.animate(withDuration: 2.0, animations: {
+			square.backgroundColor = .orange
+			let scaleTransform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+			let rotateTransform = CGAffineTransform(rotationAngle: .pi)
+			let translateTransform = CGAffineTransform(translationX: 200, y: 200)
+			let comboTransform = scaleTransform.concatenating(rotateTransform).concatenating(translateTransform)
+			square.transform = comboTransform
+		}) { _ in
+			// Revert back
+			UIView.animate(withDuration: 2.0, animations: {
+				square.backgroundColor = .purple
+				square.transform = CGAffineTransform.identity
+			})
+		}
+	}
 }
-
