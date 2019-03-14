a624f713f5ce272fc34f301e04ed10cc2cb6a817
Román Martínez <roman.mx3@gmail.com>
2019-03-14 17:07:11 -0600

    Moved info from "iTunesSearch" into "SearchBar"

--- 05. Working with the web/SearchBar/SearchBar/ViewController.swift
+++ 05. Working with the web/SearchBar/SearchBar/ViewController.swift
@@ -8,13 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController, UISearchBarDelegate {
+	
+	
+	// Initialize
+	// ------------------------------
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		setupSearchBar()
 	}
-
-
+	
+	
+	// Current terms
+	// ------------------------------
+	var currentSegment: String {
+		return segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex ) ?? ""
+	}
+	var currentKeyword: String {
+		return searchBar.text ?? ""
+	}
+	func getKeywordCharacter(at index: Int) -> Character {
+		let stringIndex = currentKeyword.index(currentKeyword.startIndex, offsetBy: index)
+		return currentKeyword[stringIndex]
+	}
+	
+	
+	// UISegmentedControl
+	// ------------------------------
+	// Use the segmented control element to provide a tab-like group of buttons
+	// you can get the index of the currently selected segment by accessing the "selectedSegmentIndex" property
+	@IBOutlet weak var segmentedControl: UISegmentedControl!
+	@IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
+		tableView.reloadData()
+	}
+	
+	
+	// UISearchBar
+	// ------------------------------
+	// Add a search bar item to allow users to enter keywords to customize a list of search results
+	// this element doesn't inherit from UIControl, so to detect any triggerd events
+	// you'll need to add a delegate item that conforms to the "UISearchBarDelegate" protocol
+	@IBOutlet weak var searchBar: UISearchBar!
+	func setupSearchBar() {
+		searchBar.delegate = self
+	}
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		if !currentKeyword.isEmpty {
+			tableView.reloadData()
+		}
+	}
+	
+	
+	// Populate table data
+	// ------------------------------
+	
+	// Sections
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String {
+		return "Searching \(currentSegment)"
+	}
+	
+	// Rows/cells
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return currentKeyword.count
+	}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
+		cell.textLabel?.text = "Item #\(indexPath.row + 1) - \(getKeywordCharacter(at: indexPath.row))"
+		return cell
+	}
+	
+	
 }
-
