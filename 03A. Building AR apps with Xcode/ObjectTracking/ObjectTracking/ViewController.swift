c0ece36e3022e036c91ea8fa2937f77f403972ee
Román Martínez <roman.mx3@gmail.com>
2019-03-18 23:21:53 -0600

    Moved info from "PlaneDetection" into "ObjectTracking"

--- 03A. Building AR apps with Xcode/ObjectTracking/ObjectTracking/ViewController.swift
+++ 03A. Building AR apps with Xcode/ObjectTracking/ObjectTracking/ViewController.swift
@@ -11,65 +11,79 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
+	
+	// Initialize
+	// ------------------------------
+	@IBOutlet var sceneView: ARSCNView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		sceneView.delegate = self
+		sceneView.showsStatistics = true
+	}
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		let configuration = ARWorldTrackingConfiguration()
+		// Setup object tracking
+		setupObjectsTracking(configuration)
+		sceneView.session.run(configuration)
+	}
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		sceneView.session.pause()
+	}
+	
+	
+	// Setup object tracking
+	// ------------------------------
+	// When object tracking is enabled, ARKit will automatically add "anchor" elements wherever the specified type of object is detected
+	// it also creates a node element so you can insert 3D objects relative to the detected anchor
+	// Use the "renderer" delegate methods to handle the detection events
+	func setupObjectsTracking(_ config: ARWorldTrackingConfiguration) {
+		setupPlaneDetection(config)
+	}
+	
+	// This method is triggered whenever an object is first detected
+	func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+		switch anchor {
+		case is ARPlaneAnchor:
+			addPlane(to: node, using: anchor as! ARPlaneAnchor)
+		default:
+			break
+		}
+	}
+	
+	// This method is triggered whenever an existing object is updated (because the object itself moved, or because ARKit improved its position or dimensions)
+	func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+		switch anchor {
+		case is ARPlaneAnchor:
+			updatePlane(on: node, using: anchor as! ARPlaneAnchor)
+		default:
+			break
+		}
+	}
+	
+	
+	// Plane detection
+	// ------------------------------
+	func setupPlaneDetection(_ config: ARWorldTrackingConfiguration) {
+		config.planeDetection = [.horizontal, .vertical]
+	}
+	func addPlane(to node: SCNNode, using anchor: ARPlaneAnchor) {
+		let planeNode = SCNNode()
+		let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+		planeNode.geometry = plane
+		planeNode.eulerAngles.x = -.pi / 2
+		planeNode.opacity = 0.25
+		node.addChildNode(planeNode)
+	}
+	func updatePlane(on node: SCNNode, using anchor: ARPlaneAnchor) {
+		guard let planeNode = node.childNodes.first else { return }
+		planeNode.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+		guard let plane = planeNode.geometry as? SCNPlane else { return }
+		plane.width = CGFloat(anchor.extent.x)
+		plane.height = CGFloat(anchor.extent.z)
+	}
+	
+	
 }
